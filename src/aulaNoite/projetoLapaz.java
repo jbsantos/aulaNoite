ce2ddd166db54d90a504811d4f13059f60726305
jbsantos <jorge0302@gmail.com>
2015-10-09 14:44:10 -0300

    Deus é

--- src/aulaNoite/projetoLapaz.java
+++ src/aulaNoite/projetoLapaz.java
@@ -5,7 +5,7 @@ public class projetoLapaz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String nome;
-		nome="sdf";
+		nome="sdfsdfgh";
 		System.out.println(nome);
 	}
 
