0e6dd7dedbaa862da89913ddf6077edd43b5244b
jbsantos <jorge0302@gmail.com>
2015-10-09 14:41:30 -0300

    Deus é fiel

---
+++ src/aulaNoite/projetoLapaz.java
@@ -0,0 +1,12 @@
+package aulaNoite;
+
+public class projetoLapaz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome;
+		nome="sdf";
+		System.out.println(nome);
+	}
+
+}
